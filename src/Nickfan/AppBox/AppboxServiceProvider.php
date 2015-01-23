51a40812be7cf1b77d69680fe4063a7597ac6f20
nick fan <nickfan81@gmail.com>
2015-01-23 16:13:46 +0800

    add laravel support

---
+++ src/Nickfan/AppBox/AppboxServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Nickfan\Appbox;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppboxServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('nickfan/appbox');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
